d4e6fddef252b37a45e877a3c73184b2c07e2f6a
LiHeQian6 <1142808063@qq.com>
2019-09-21 18:43:53 +0800

    init

---
+++ Traveldiary/app/src/main/java/com/project/li/travel_diary/MainActivity.java
@@ -0,0 +1,13 @@
+package com.project.li.travel_diary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
