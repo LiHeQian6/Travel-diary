382277a1dd5ebfeea29426a75716cd09f49e5b97
859684581@qq.com <ziyousjw5211314>
2019-09-24 09:23:53 +0800

    sunjianwang

---
+++ Traveldiary/app/src/main/java/com/project/li/travel_diary/LoginActivity.java
@@ -0,0 +1,14 @@
+package com.project.li.travel_diary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+    }
+}
