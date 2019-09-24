3b9e1a08b12bb425d2b3fd558bc84cf31d4bc6ff
859684581@qq.com <ziyousjw5211314>
2019-09-24 10:36:16 +0800

    修改首先需登录注册

--- Traveldiary/app/src/main/java/com/project/li/travel_diary/LoginActivity.java
+++ Traveldiary/app/src/main/java/com/project/li/travel_diary/LoginActivity.java
@@ -1,14 +1,28 @@
 package com.project.li.travel_diary;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private LinearLayout login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
+        login = findViewById(R.id.btnLogin);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent();
+                intent.setClass(LoginActivity.this,MainPageActivity.class);
+                startActivity(intent);
+            }
+        });
     }
+
 }
