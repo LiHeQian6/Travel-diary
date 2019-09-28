d76ee0cf4cb50ac68a58d1c0105a0447e83be707
859684581@qq.com <ziyousjw5211314>
2019-09-28 19:24:37 +0800

    完成注册页面及其部分功能

--- Traveldiary/app/src/main/java/com/project/li/travel_diary/LoginActivity.java
+++ Traveldiary/app/src/main/java/com/project/li/travel_diary/LoginActivity.java
@@ -2,33 +2,33 @@ package com.project.li.travel_diary;
 
 import android.content.Intent;
 import android.os.Build;
+import android.support.annotation.RequiresApi;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 
 public class LoginActivity extends AppCompatActivity {
 
     private LinearLayout login;
     private Button btnRegister;
+    private EditText Emaiaddress;
+    private EditText password;
+    private String emaiaddress;
+    private String pass;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
         setStatusBar();
-        login = findViewById(R.id.btnLogin);
-        btnRegister = findViewById(R.id.btnRegister);
-        login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setClass(LoginActivity.this,MainPageActivity.class);
-                startActivity(intent);
-            }
-        });
+        getView();
+        textChange();
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -37,6 +37,94 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+    }
+
+    protected void textChange(){
+        /**
+         * 输入账号文本框监听器
+         */
+        Emaiaddress.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @RequiresApi(api = Build.VERSION_CODES.JELLY_BEAN)
+            @Override
+            public void afterTextChanged(Editable s) {
+                emaiaddress = Emaiaddress.getText().toString().trim();
+                pass = password.getText().toString().trim();
+                if(!emaiaddress.equals("") && !pass.equals("")){
+                    login.setBackgroundDrawable(getResources().getDrawable(R.drawable.btnloginstyle));
+                    login.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intent = new Intent();
+                            intent.setClass(LoginActivity.this,MainPageActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    });
+                }else{
+                    login.setBackgroundDrawable(getResources().getDrawable(R.drawable.loginstyle));
+                    login.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                        }
+                    });
+                }
+            }
+        });
+        /**
+         * 输入密码文本框监听器
+         */
+        password.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                emaiaddress = Emaiaddress.getText().toString().trim();
+                pass = password.getText().toString().trim();
+                if(!emaiaddress.equals("") && !pass.equals("")){
+                    login.setBackgroundDrawable(getResources().getDrawable(R.drawable.btnloginstyle));
+                    login.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            Intent intent = new Intent();
+                            intent.setClass(LoginActivity.this,MainPageActivity.class);
+                            startActivity(intent);
+                            finish();
+                        }
+                    });
+                }else{
+                    login.setBackgroundDrawable(getResources().getDrawable(R.drawable.loginstyle));
+                    login.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                        }
+                    });
+                }
+            }
+        });
+    }
+
+    protected void getView(){
+        login = findViewById(R.id.btnLogin);
+        btnRegister = findViewById(R.id.btnRegister);
+        Emaiaddress = findViewById(R.id.edtUsername);
+        password = findViewById(R.id.edtPassword);
     }
 
     protected void setStatusBar() {
--- Traveldiary/app/src/main/java/com/project/li/travel_diary/RegisterActivity.java
+++ Traveldiary/app/src/main/java/com/project/li/travel_diary/RegisterActivity.java
@@ -3,15 +3,186 @@ package com.project.li.travel_diary;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class RegisterActivity extends AppCompatActivity {
-
+    private EditText edtEmailAddress;
+    private EditText edtPassword;
+    private EditText edtAgainPassword;
+    private EditText edtVerifyCode;
+    private TextView edtgetVerifyCode;
+    private Button btnNext;
+    private String emailAddress;
+    private String password;
+    private String againPassword;
+    private String VerifyCode;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
         setStatusBar();
+
+        getView();
+        textChange();
+    }
+    protected void textChange(){
+        /**
+         * 输入账号文本框监听器
+         */
+        edtEmailAddress.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                emailAddress = edtEmailAddress.getText().toString().trim();
+                password = edtPassword.getText().toString().trim();
+                againPassword = edtAgainPassword.getText().toString().trim();
+                VerifyCode = edtVerifyCode.getText().toString().trim();
+                if(!emailAddress.equals("") && !password.equals("") && !againPassword.equals("") && !VerifyCode.equals("")){
+                    btnNext.setBackgroundDrawable(getResources().getDrawable(R.drawable.registerforstyle));
+                    btnNext.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                        }
+                    });
+                }else{
+                    btnNext.setBackgroundDrawable(getResources().getDrawable(R.drawable.registerstyle));
+                    btnNext.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                        }
+                    });
+                }
+            }
+        });
+        /**
+         * 输入密码文本框监听器
+         */
+        edtPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                emailAddress = edtEmailAddress.getText().toString().trim();
+                password = edtPassword.getText().toString().trim();
+                againPassword = edtAgainPassword.getText().toString().trim();
+                VerifyCode = edtVerifyCode.getText().toString().trim();
+                if(!emailAddress.equals("") && !password.equals("") && !againPassword.equals("") && !VerifyCode.equals("")){
+                    btnNext.setBackgroundDrawable(getResources().getDrawable(R.drawable.registerforstyle));
+                    btnNext.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                        }
+                    });
+                }else{
+                    btnNext.setBackgroundDrawable(getResources().getDrawable(R.drawable.registerstyle));
+                    btnNext.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                        }
+                    });
+                }
+            }
+        });
+        /**
+         * 再次输入密码文本框监听器
+         */
+        edtAgainPassword.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                emailAddress = edtEmailAddress.getText().toString().trim();
+                password = edtPassword.getText().toString().trim();
+                againPassword = edtAgainPassword.getText().toString().trim();
+                VerifyCode = edtVerifyCode.getText().toString().trim();
+                if(!emailAddress.equals("") && !password.equals("") && !againPassword.equals("") && !VerifyCode.equals("")){
+                    btnNext.setBackgroundDrawable(getResources().getDrawable(R.drawable.registerforstyle));
+                    btnNext.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                        }
+                    });
+                }else{
+                    btnNext.setBackgroundDrawable(getResources().getDrawable(R.drawable.registerstyle));
+                    btnNext.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                        }
+                    });
+                }
+            }
+        });
+        /**
+         * 输入验证码文本框监听器
+         */
+        edtVerifyCode.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+                emailAddress = edtEmailAddress.getText().toString().trim();
+                password = edtPassword.getText().toString().trim();
+                againPassword = edtAgainPassword.getText().toString().trim();
+                VerifyCode = edtVerifyCode.getText().toString().trim();
+                if(!emailAddress.equals("") && !password.equals("") && !againPassword.equals("") && !VerifyCode.equals("")){
+                    btnNext.setBackgroundDrawable(getResources().getDrawable(R.drawable.registerforstyle));
+                    btnNext.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                        }
+                    });
+                }else{
+                    btnNext.setBackgroundDrawable(getResources().getDrawable(R.drawable.registerstyle));
+                    btnNext.setOnClickListener(new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                        }
+                    });
+                }
+            }
+        });
+    }
+    protected void getView(){
+        edtEmailAddress = findViewById(R.id.edtEmailaddress);
+        edtPassword = findViewById(R.id.edtPassword);
+        edtAgainPassword = findViewById(R.id.edtAgainPass);
+        edtVerifyCode = findViewById(R.id.edtVerifyCode);
+        edtgetVerifyCode = findViewById(R.id.edtGetVerifyCode);
+        btnNext = findViewById(R.id.btnNext);
     }
     protected void setStatusBar() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
