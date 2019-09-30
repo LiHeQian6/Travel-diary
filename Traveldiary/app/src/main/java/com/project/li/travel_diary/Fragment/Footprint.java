3a9e4bbd47414a85649839d8db095bbb4c7721b9
LiHeQian6 <1142808063@qq.com>
2019-09-30 19:21:52 +0800

    集成高德地图、签名
    Signed-off-by: LiHeQian6 <1142808063@qq.com>

--- Traveldiary/app/src/main/java/com/project/li/travel_diary/Fragment/Footprint.java
+++ Traveldiary/app/src/main/java/com/project/li/travel_diary/Fragment/Footprint.java
@@ -3,18 +3,64 @@ package com.project.li.travel_diary.Fragment;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.amap.api.maps.AMap;
+import com.amap.api.maps.MapView;
+import com.amap.api.maps.SupportMapFragment;
+import com.amap.api.maps.model.MyLocationStyle;
 import com.project.li.travel_diary.R;
 
-public class Footprint extends Fragment {
+public class Footprint extends SupportMapFragment {
+    private MapView mapView;
+    private AMap map;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.footprint_layout,container,false);
+        View view = inflater.inflate(R.layout.footprint_layout, container, false);
+        mapView=view.findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        if (map == null) {
+            map = mapView.getMap();
+        }
+        MyLocationStyle locationStyle = new MyLocationStyle();
+        locationStyle.myLocationType(MyLocationStyle.LOCATION_TYPE_LOCATION_ROTATE);
+        map.setMyLocationStyle(locationStyle);
+        map.getUiSettings().setMyLocationButtonEnabled(true);
+        map.setMyLocationEnabled(true);
         return view;
     }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle bundle) {
+        super.onSaveInstanceState(bundle);
+        mapView.onSaveInstanceState(bundle);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
 }
