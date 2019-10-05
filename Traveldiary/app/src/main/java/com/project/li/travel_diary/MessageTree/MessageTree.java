694a8cecf8dbed12283039d95cc05b99c06d11ef
sunyufei666 <156267102@qq.com>
2019-10-05 22:08:04 +0800

    2019-10-5 22:07 修改

--- Traveldiary/app/src/main/java/com/project/li/travel_diary/MessageTree/MessageTree.java
+++ Traveldiary/app/src/main/java/com/project/li/travel_diary/MessageTree/MessageTree.java
@@ -1,9 +1,13 @@
 package com.project.li.travel_diary.MessageTree;
 
+import android.content.Context;
 import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import android.view.View;
+import android.view.WindowManager;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 
 import com.project.li.travel_diary.R;
@@ -17,23 +21,29 @@ public class MessageTree extends AppCompatActivity {
     private List<Map<String,String>> dataSource;
     private ListView listView;
     private TreeAdapter treeAdapter;
+    private int displayWidth;
+    private int displayHeight;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message_tree);
 
         setStatusBar();
+
+
         dataSource = new ArrayList<>();
         Map<String,String> item = new HashMap<>();
-        item.put("time","2019.10.2");
+        item.put("leavedate","2019.10.2");
         item.put("location","拉斯维加斯");
         item.put("message","哈哈哈哈哈哈哈哈哈哈");
+        item.put("finger","456");
         dataSource.add(item);
         listView = findViewById(R.id.list_item);
         treeAdapter = new TreeAdapter(this,dataSource,R.layout.tree_item);
         listView.setAdapter(treeAdapter);
 
     }
+
     protected void setStatusBar() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             //getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);//隐藏状态栏但不隐藏状态栏字体
@@ -42,4 +52,6 @@ public class MessageTree extends AppCompatActivity {
 
         }
     }
+
+
 }
--- Traveldiary/app/src/main/java/com/project/li/travel_diary/MessageTree/TreeAdapter.java
+++ Traveldiary/app/src/main/java/com/project/li/travel_diary/MessageTree/TreeAdapter.java
@@ -2,10 +2,14 @@ package com.project.li.travel_diary.MessageTree;
 
 import android.content.Context;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.project.li.travel_diary.R;
@@ -17,6 +21,8 @@ public class TreeAdapter extends BaseAdapter {
     private Context context;    // 上下文环境
     private List<Map<String, String>> dataSource; // 声明数据源
     private int item_layout_id; // 声明列表项的布局
+    private LinearLayout messageLayoutEd;
+    private LinearLayout messageLayout;
     // 声明列表项中的控件
     public TreeAdapter(Context context, List<Map<String, String>> dataSource,
                          int item_layout_id) {
@@ -37,10 +43,63 @@ public class TreeAdapter extends BaseAdapter {
         }
 
         final Map<String,String> textFull = dataSource.get(position);
-        TextView topTip = convertView.findViewById(R.id.toptip);
-        TextView lMessage = convertView.findViewById(R.id.lmessage);
-        topTip.setText(textFull.get("time")+"，你在"+textFull.get("location")+"留下了");
-        lMessage.setText(textFull.get("message"));
+
+        //textview框
+        TextView location = convertView.findViewById(R.id.location);
+        final TextView message = convertView.findViewById(R.id.message);
+        TextView leaveDate = convertView.findViewById(R.id.leavedate);
+        TextView finger = convertView.findViewById(R.id.finger);
+        ImageView trash = convertView.findViewById(R.id.trash);
+        ImageView rewrite = convertView.findViewById(R.id.rewrite);
+
+        //editview框
+        TextView locationEd = convertView.findViewById(R.id.location_ed);
+        final TextView messageEd = convertView.findViewById(R.id.message_ed);
+        TextView leaveDateEd = convertView.findViewById(R.id.leavedate_ed);
+        TextView fingerEd = convertView.findViewById(R.id.finger_ed);
+        ImageView trashEd = convertView.findViewById(R.id.trash_ed);
+        ImageView save = convertView.findViewById(R.id.save);
+
+        location.setText(textFull.get("location"));
+        message.setText(textFull.get("message"));
+        leaveDate.setText(textFull.get("leavedate"));
+        finger.setText(textFull.get("finger"));
+        messageLayout = convertView.findViewById(R.id.message_layout);
+        messageLayoutEd = convertView.findViewById(R.id.message_layout_ed);
+
+
+
+        rewrite.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                messageLayoutEd.setVisibility(View.VISIBLE);
+                messageLayout.setVisibility(View.GONE);
+            }
+        });
+        trash.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dataSource.remove(position);
+                notifyDataSetChanged();
+            }
+        });
+
+        save.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e("调试信息","执行");
+                notifyDataSetChanged();
+                messageLayout.setVisibility(View.VISIBLE);
+                messageLayoutEd.setVisibility(View.GONE);
+            }
+        });
+        trashEd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dataSource.remove(position);
+                notifyDataSetChanged();
+            }
+        });
 
         return convertView;
     }
